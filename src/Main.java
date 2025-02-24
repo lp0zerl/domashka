c23994fb6ab1e9708a009e43dcbddab18a3025c2
Lenovo <kontumer16@gmail.com>
2025-02-25 02:41:08 +0300

    первое задание переменные

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,10 @@ public class Main {
     public static void main(String[] args) {
         var dog = 8.0;      // переменная dog будет иметь тип double
         var cat = 3.6;     // переменная cat будет иметь тип double
-        var paper = 76378; // переменная paper будет иметь тип int
+        var paper = 76378;  // переменная paper будет иметь тип int
 
-        // Увеличиваем значения переменных на 4
-        dog += 4;
-        cat += 4;
-        paper += 4;
-
-        // Выводим новые значения переменных в консоль
-        System.out.println("Новое значение dog = " + dog);
-        System.out.println("Новое значение cat = " + cat);
-        System.out.println("Новое значение paper = " + paper);
+        System.out.println("dog = " + dog);
+        System.out.println("cat = " + cat);
+        System.out.println("paper = " + paper);
     }
 }
