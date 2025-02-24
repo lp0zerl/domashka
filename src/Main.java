d06a0db3affc22048ac372dd76c7db61e3ba010d
Lenovo <kontumer16@gmail.com>
2025-02-25 02:33:59 +0300

    первое задание переменные

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        var dog = 8.0;      // переменная dog будет иметь тип double
+        var cat = 3.6;     // переменная cat будет иметь тип double
+        var paper = 76378;  // переменная paper будет иметь тип int
+
+        System.out.println("dog = " + dog);
+        System.out.println("cat = " + cat);
+        System.out.println("paper = " + paper);
+    }
+}
